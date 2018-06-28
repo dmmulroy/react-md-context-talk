2290b8a1897dc6686df57895f8f02fe1bd0eb136
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2018-06-28 15:57:53 -0400

    Update slides w/ pdf, make minor context code changes

---
+++ app/src/context/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Todo from './Todo';
+import visibilityFilters from '../constants/visibilityFilters';
+
+const getFilteredTodos = (todos, visibilityFilter) => {
+  switch (visibilityFilter) {
+    case visibilityFilters.SHOW_ALL:
+      return todos;
+    case visibilityFilters.SHOW_ACTIVE:
+      return todos.filter(({ completed }) => !completed);
+    case visibilityFilters.SHOW_COMPLETED:
+      return todos.filter(({ completed }) => completed);
+    default:
+      return todos;
+  }
+};
+
+const TodoList = ({ todos, visibilityFilter }) => {
+  const filteredTodos = getFilteredTodos(todos, visibilityFilter);
+
+  return <ul>{filteredTodos.map(todo => <Todo key={todo.id} {...todo} />)}</ul>;
+};
+
+export default TodoList;
