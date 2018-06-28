0024182dce6ae9a9973be99f41eff7e3fde9c950
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2018-06-28 17:21:02 -0400

    Move todo.txt to initial state of redux app

--- app/src/redux/reducers/todos.js
+++ app/src/redux/reducers/todos.js
@@ -1,4 +1,33 @@
-const todos = (state = [], action) => {
+const initialState = [
+  {
+    id: 0,
+    text: 'Create the Todo Context (Consumer + Provider) + TodoContextProvider',
+    completed: false
+  },
+  {
+    id: 1,
+    text: 'Show Completed VisibilityFilter Context + TodoContextProvider',
+    completed: false
+  },
+  {
+    id: 2,
+    text: 'Add Providers to the Root/Index Component',
+    completed: false
+  },
+  {
+    id: 3,
+    text:
+      'Finish creating the VisbileTodoList (Visibility + Todo Consumers) and Todo Components (Todo Consumer for toggleTodo)',
+    completed: false
+  },
+  {
+    id: 4,
+    text: 'Show Completed VisibilityFilters + VisibilityFilterButton',
+    completed: false
+  }
+];
+
+const todos = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -8,16 +37,15 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false
         }
-      ]
+      ];
     case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id)
-          ? {...todo, completed: !todo.completed}
-          : todo
-      )
+      return state.map(
+        todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default todos
+export default todos;
